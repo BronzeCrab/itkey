09cc30f5f726350a2b0dd05bb09462206622db7c
Alex <gr4k@bk.ru>
2015-04-01 15:04:56 +0300

    Yaroslav task backup commit1

---
+++ backup.sh
@@ -0,0 +1,28 @@
+#/!bin/bash
+
+# Testing
+#var='hello'
+#echo $var
+# Путь к директории
+#/home/gr4k/backup/test1
+
+# Нужно забэкапить что-то используя tar -cf
+
+# Переменная которая хранит имя архива
+var1=$1
+echo $var1
+
+# Переменная с датой
+date_var=$(date +%g%m%d-%H%M) 
+echo $date_var
+
+#Изменяю переменную с именем архива
+var1="${var1/".tar"/""}"
+var1="$var1.$date_var.tar"
+echo $var1
+
+# Переменная путь к архиву
+var2=$2
+echo $var2
+
+tar -cf $var1 $var2
