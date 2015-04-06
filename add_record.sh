2ca269b9ca21775189fe85cdac2b5d8ad88c1ae7
Alexey Ustinnikov <gr4k@bk.ru>
2015-04-06 18:00:13 +0300

    6.04 version

--- add_record.sh
+++ add_record.sh
@@ -12,24 +12,60 @@ RECORD="${HOST} IN A ${IP}"
 # validate ip
 # add error handling
 
-if [[ $# < 3 ]]; then
+# Функция для проверки IP. Сначала регулярным проверяю на то , что все элементы либо цифры 
+# либо точки, а затем уже значения октетов, чтобы не было больше 255
+
+function ip_test() {
+	temp=""
+    ip_valid=0
+    if [[ `echo ${IP} | sed 's/\([[:digit:]]\{1,3\}\.\)\{3\}[[:digit:]]\{1,3\}//g'` == "" ]]; then
+		for (( i=0; i<${#IP}; i++ )); do
+		    if [[ ${IP:$i:1} == "." ]]; then
+		  		if [[ $temp -gt 255 ]]; then
+		  			ip_valid=1
+		  		fi
+		  		temp=""
+		  	elif [[ $i == $[${#IP}-1] ]]; then
+		  		temp=$temp${IP:$i:1}
+		  		if [[ $temp -gt 255 ]]; then
+		  			ip_valid=1
+		  		fi
+		  	else
+		  		temp=$temp${IP:$i:1}			
+		  	fi
+		done
+	else
+		ip_valid=1
+	fi	
+}
+
+ip_test
+
+# Проверка остальных входных параметров 
+
+if [[ $# -lt 3 ]]; then
 	echo Слишком мало параметров!
 	exit 1
-elif [[ ! -f /etc/bind/zones/${DOMAIN}.db ]]; then
-	echo Файла c зонами не существует
+elif [[ ! -f /etc/bind/${DOMAIN}.db ]]; then
+	echo Файла c зонами не существует, запись $RECORD не добавлена
 	exit 1
-elif [[ echo ${HOST} | grep -o "\." | wc -l != 3 ]]; then
-	echo Неправильно введен ip
+elif [[ ip_valid -eq 1 ]]; then
+	echo Неправильно введен IP, запись $RECORD не добавлена
 	exit 1
-
 fi
 
-
-
 if [ "$MODE" == "1" ]; then
-        sed -i 's/'"$RECORD"'//g' /etc/bind/${DOMAIN}.db
+    if grep -xq "${RECORD}" /etc/bind/${DOMAIN}.db; then
+    	sed -i 's/'"$RECORD"'//g' /etc/bind/${DOMAIN}.db
+    	echo Успешно удалили запись $RECORD
+    else
+    	echo Не найдена запись $RECORD, не удалена
+    fi	
 elif [ "$(cat /etc/bind/${DOMAIN}.db | grep "${RECORD}")" == "" ]; then
-        echo -e $RECORD >> /etc/bind/${DOMAIN}.db
+    echo -e $RECORD >> /etc/bind/${DOMAIN}.db
+    echo Успешно добавлена запись $RECORD
+else
+	echo Повтор записи $RECORD, не добавлена
 fi
 
 service bind9 restart
--- mass_add_dns.sh
+++ mass_add_dns.sh
@@ -1,23 +1,32 @@
-# cat mass_add_dns.sh
 #!/bin/bash
-
 
 #TODO:
 # make it work
 # add validation
 # add error handling
 
+if [[ $# -lt 1 ]]; then
+	echo Нет пути к файлу, завершение
+	exit 1
+fi
+
 HOSTFILE=$1
 
-for LINE in `cat $HOSTFILE`; do
+while read line
+do  
 
-        HOSTNAME=$(echo $LINE | awk '{print $1}')
-        IP=$(echo $LINE | awk '{print $2}')
-
-        DOMAIN=$(echo $HOSTNAME | cut -d'.' -f2-)
-
-        echo $DOMAIN
-
-        #./add_record.sh $HOST $IP $DOMAIN
-
-done
+    HOSTNAME=$(echo $line | awk '{print $1}')
+    IP=$(echo $line | awk '{print $2}')
+    if [[ $(echo $HOSTNAME | grep -o "\." | wc -l) -eq 2 ]]; then
+    	DOMAIN=$(echo $HOSTNAME | cut -d'.' -f2-)
+    else
+    	DOMAIN=$(echo $HOSTNAME | cut -d'.' -f3-)
+    fi	
+    LABEL=$(echo $line | awk '{print $3}')
+    
+    if [[ $LABEL != "" ]]; then
+    	./add_record.sh $HOSTNAME $IP $DOMAIN 1
+    else
+    	./add_record.sh $HOSTNAME $IP $DOMAIN 
+    fi 
+done < $HOSTFILE
