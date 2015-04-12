6ff39d3f05b4350064642866efbb90c695201b37
gr4k <gr4k@bk.ru>
2015-04-12 17:17:19 +0300

    chenges indent 12.04

--- mass_add_dns.sh
+++ mass_add_dns.sh
@@ -14,25 +14,25 @@ HOSTFILE=$1
 
 while read line
 do
-	  echo $line
-    HOSTNAME=$(echo $line | awk '{print $1}')
-    IP=$(echo $line | awk '{print $2}')
-    # DOMAIN=$(echo $HOSTNAME | grep -o "[^\.]*\.[^\.]*$")
-		DOMAIN=$(echo $HOSTNAME | rev |cut -d'.' -f1-2 | rev)
-    LABEL=$(echo $line | awk '{print $3}')
+	echo $line
+	HOSTNAME=$(echo $line | awk '{print $1}')
+	IP=$(echo $line | awk '{print $2}')
+	# DOMAIN=$(echo $HOSTNAME | grep -o "[^\.]*\.[^\.]*$")
+	DOMAIN=$(echo $HOSTNAME | rev |cut -d'.' -f1-2 | rev)
+	LABEL=$(echo $line | awk '{print $3}')
 
-    if [[ $LABEL != "" ]]; then
-    	./add_record.sh $HOSTNAME $IP $DOMAIN 1
-    else
-    	./add_record.sh $HOSTNAME $IP $DOMAIN
-    fi
+	if [[ $LABEL != "" ]]; then
+		./add_record.sh $HOSTNAME $IP $DOMAIN 1
+	else
+		./add_record.sh $HOSTNAME $IP $DOMAIN
+	fi
 done < $HOSTFILE
 
 
 
 
- # if [[ $(echo $HOSTNAME | grep -o "\." | wc -l) -eq 2 ]]; then
- #    	DOMAIN=$(echo $HOSTNAME | cut -d'.' -f2-)
- #    else
- #    	DOMAIN=$(echo $HOSTNAME | cut -d'.' -f3-)
- #    fi
+# if [[ $(echo $HOSTNAME | grep -o "\." | wc -l) -eq 2 ]]; then
+#    	DOMAIN=$(echo $HOSTNAME | cut -d'.' -f2-)
+#    else
+#    	DOMAIN=$(echo $HOSTNAME | cut -d'.' -f3-)
+#    fi
